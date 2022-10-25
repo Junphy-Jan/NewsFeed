1ade29c3c51e6fdb28ec1f6b8b7479edf0b50440
Joshua <microjan@outlook.com>
2022-10-25 23:08:50 +0800

    增加网页版阅读界面

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ from newsFeedUtils.loggerHelper import logger
 app = Flask(__name__)
 
 
+# 存放每个用户对应的随机新闻顺序
+user_news_idx = {}
+
+
 def load_news():
     print(os.getcwd())
     paper_news_relative_path = "static/news/paper_version"
@@ -55,29 +59,47 @@ def shuffle_news(news_list):
 #     return 'Hello World!'
 @app.route('/newspaper/<uid>')
 def newspaper(uid):
-    # global all_news_list
-    shuffle_idx = [i for i in range(len(all_paper_news_list))]
-    random.shuffle(shuffle_idx)
+    global user_news_idx
+    if uid in user_news_idx:
+        shuffle_idx = user_news_idx[uid]
+    else:
+        shuffle_idx = [i for i in range(len(all_paper_news_list))]
+        random.shuffle(shuffle_idx)
+        user_news_idx[uid] = shuffle_idx
     logger.info("user: {} visits newspaper version with news sequence: {}".format(escape(uid), shuffle_idx))
     news = [all_paper_news_list[i] for i in shuffle_idx]
     # all_news_list = shuffle_news(all_news_list)
-    return render_template('newspaper.html', news_list=news)
+    return render_template('newspaper.html', news_list=news, uid=uid)
+
+
+@app.route('/newspaper/newspaper_read/<news_idx>/<uid>')
+def read_news(news_idx, uid):
+    news_idx = int(news_idx)
+    # news_idx = user_news_idx[uid][news_idx]
+    news_list = [all_paper_news_list[i] for i in user_news_idx[uid]]
+    return render_template('newspaper_read.html', news_idx=news_idx, news_list=news_list)
 
 
 @app.route('/web_news/<uid>')
 def web_news(uid):
     # global all_news_list
-    shuffle_idx = [i for i in range(len(all_web_news_list))]
-    random.shuffle(shuffle_idx)
+    global user_news_idx
+    if uid in user_news_idx:
+        shuffle_idx = user_news_idx[uid]
+    else:
+        shuffle_idx = [i for i in range(len(all_paper_news_list))]
+        random.shuffle(shuffle_idx)
+        user_news_idx[uid] = shuffle_idx
     logger.info("user: {} visits web version with news sequence: {}".format(escape(uid), shuffle_idx))
     news = [all_web_news_list[i] for i in shuffle_idx]
-    return render_template('web-format.html', news_list=news)
+    return render_template('web-format-overview.html', news_list=news, uid=uid)
 
 
-@app.route('/newspaper/newspaper_read/<news_idx>')
-def read_news(news_idx):
+@app.route('/web_news/web_news_read/<news_idx>/<uid>')
+def read_web_news(news_idx, uid):
     news_idx = int(news_idx)
-    return render_template('newspaper_read.html', news_idx=news_idx, news_list=all_paper_news_list)
+    news_list = [all_paper_news_list[i] for i in user_news_idx[uid]]
+    return render_template('web-format-read.html', news=news_list[news_idx])
 
 
 if __name__ == '__main__':

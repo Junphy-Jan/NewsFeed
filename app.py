95710d821a5fba0a96a6cd0dcaa7ec53270ab979
Joshua <microjan@outlook.com>
2022-11-03 20:22:43 +0800

    禁止返回以及用户访问数据

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request
 from markupsafe import escape
 from flask_socketio import SocketIO
 from newsFeedUtils.loggerHelper import logger, hb_logger
+from userStatistical import UserData, Channel
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -15,6 +16,7 @@ socketio = SocketIO(app)
 
 # 存放每个用户对应的随机新闻顺序
 user_news_idx = {}
+user_statistical_data = {}
 
 
 def load_news():
@@ -62,18 +64,21 @@ def shuffle_news(news_list):
 #     return 'Hello World!'
 @app.route('/newspaper/<uid>')
 def newspaper(uid):
-    logger.info("request: {}".format(request.data))
-    logger.info("request.head: {}".format(dict(request.headers)))
+    # logger.info("request: {}".format(request.data))
+    # logger.info("request.head: {}".format(dict(request.headers)))
     global user_news_idx
-    # if uid in user_news_idx:
-    #     shuffle_idx = user_news_idx[uid]
-    # else:
-    #     shuffle_idx = [i for i in range(len(all_paper_news_list))]
-    #     random.shuffle(shuffle_idx)
-    #     user_news_idx[uid] = shuffle_idx
+    # 同一用户刷新时不会重新随机新闻顺序
+    if uid in user_news_idx:
+        shuffle_idx = user_news_idx[uid]
+    else:
+        shuffle_idx = [i for i in range(len(all_paper_news_list))]
+        random.shuffle(shuffle_idx)
+        user_news_idx[uid] = shuffle_idx
+        user_data = UserData(uid, time.time(), Channel.paper_news.name, shuffle_idx)
+        user_statistical_data[uid] = user_data
 
-    shuffle_idx = [i for i in range(len(all_paper_news_list))]
-    random.shuffle(shuffle_idx)
+    # shuffle_idx = [i for i in range(len(all_paper_news_list))]
+    # random.shuffle(shuffle_idx)
     # if shuffle_idx[-1] == 2:
     #     tmp = shuffle_idx[2]
     #     shuffle_idx[2] = 2
@@ -94,13 +99,8 @@ def read_news(news_idx, uid):
     news_idx = int(news_idx)
     # news_idx = user_news_idx[uid][news_idx]
     news_list = [all_paper_news_list[i] for i in user_news_idx[uid]]
+    user_statistical_data[uid].update_click_data(user_news_idx[uid][news_idx])
     return render_template('newspaper_read-v3.html', news_idx=news_idx, news_list=news_list)
-
-
-@app.route('/click')
-def click_data():
-    logger.info("点击事件|args:{}".format(request.args))
-    return "OK"
 
 
 @app.route('/web_news/<uid>')
@@ -115,6 +115,8 @@ def web_news(uid):
         shuffle_idx = [i for i in range(len(all_paper_news_list))]
         random.shuffle(shuffle_idx)
         user_news_idx[uid] = shuffle_idx
+        user_data = UserData(uid, time.time(), Channel.web_news.name, shuffle_idx)
+        user_statistical_data[uid] = user_data
     logger.info("user: {} visits web version with news sequence: {}".format(escape(uid), shuffle_idx))
     news = [all_web_news_list[i] for i in shuffle_idx]
     return render_template('web-format-overview.html', news_list=news, uid=uid)
@@ -124,7 +126,41 @@ def web_news(uid):
 def read_web_news(news_idx, uid):
     news_idx = int(news_idx)
     news_list = [all_paper_news_list[i] for i in user_news_idx[uid]]
+    user_statistical_data[uid].update_click_data(user_news_idx[uid][news_idx])
     return render_template('web-format-read.html', news=news_list[news_idx])
+
+
+@app.route('/click')
+def click_data():
+    data = request.args
+    logger.info("点击事件|args:{}".format(data))
+    for i in range(len(all_paper_news_list)):
+        if all_paper_news_list[i]["title"] == data["newsTitle"]:
+            user_statistical_data[data["userName"]].update_click_data(i)
+    return "OK"
+
+
+@app.route('/leave')
+def leave_data():
+    data = request.args
+    logger.info("点击事件|args:{}".format(data))
+    user_statistical_data[data["userName"]].update_end_time(time.time())
+    return "OK"
+
+
+@app.route('/switch_tab')
+def switch_tab():
+    data = request.args
+    logger.info("切换标签页|args:{}".format(data))
+    user_statistical_data[data["userName"]].update_switch_tab()
+    return "OK"
+
+
+@app.route('/user_data')
+def get_user_data():
+    cur_data = [user_statistical_data[user].get_info() for user in user_statistical_data]
+    # print(cur_data)
+    return json.dumps(cur_data)
 
 
 @socketio.on('heartbeat')
--- userStatistical.py
+++ userStatistical.py
@@ -1,4 +1,9 @@
+import enum
 import time
+from enum import Enum
+
+
+time_format = "%Y-%m-%d %H:%M:%S"
 
 
 class UserData:
@@ -9,6 +14,7 @@ class UserData:
         self.news_seq = news_seq
         self.end_time = None
         self.visit_data = []
+        self.switch_tab = False
 
     def update_end_time(self, end_time):
         self.end_time = end_time
@@ -16,3 +22,22 @@ class UserData:
     def update_click_data(self, original_news_id):
         self.visit_data.append((original_news_id, time.time()))
 
+    def update_switch_tab(self):
+        self.switch_tab = True
+
+    def get_info(self):
+        return {"uid": self.user_id, "startTime": time.strftime(time_format, time.localtime(self.start_time)),
+                "channel": self.channel, "newsSeq": self.news_seq,
+                "endTime": time.strftime(time_format, time.localtime(self.end_time)) if self.end_time is not None else None,
+                "visitData": [{"newsId": news_id, "time": time.strftime(time_format, time.localtime(visit_time))} for news_id, visit_time in self.visit_data],
+                "switch_tab": self.switch_tab}
+
+
+class Channel(Enum):
+    paper_news = enum.auto()
+    web_news = enum.auto()
+
+
+if __name__ == '__main__':
+    print(Channel.paper_news.value)
+    print(Channel.web_news.value)

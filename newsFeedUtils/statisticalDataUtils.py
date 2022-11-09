57edc9e43a849ba8c93fe03dc627b094856a871c
Joshua <microjan@outlook.com>
2022-11-10 00:39:11 +0800

    从心跳日志中获取用户访问时长

--- newsFeedUtils/statisticalDataUtils.py
+++ newsFeedUtils/statisticalDataUtils.py
@@ -8,10 +8,26 @@ def read_json_data(filepath):
     return json_data
 
 
+def get_duration_from_hb():
+    user_duration = {}
+    with open("../attachment/heartbeat.log", encoding="utf-8") as f:
+        for line in f:
+            heartbeat = line.strip("\n").split("|")[1]
+            heartbeat = json.loads(heartbeat.replace("'", "\""))
+            user = heartbeat["username"]
+            if user not in user_duration:
+                user_duration[user] = {"start_time": heartbeat["time"]}
+            else:
+                user_duration[user]["end_time"] = heartbeat["time"]
+                user_duration[user]["duration_from_hb"] = int(user_duration[user]["end_time"] - user_duration[user]["start_time"])
+    return user_duration
+
+
 def transform2excel(dict_data):
     import pandas as pd
     import datetime
     date_format = "%Y-%m-%d %H:%M:%S"
+    user_duration = get_duration_from_hb()
     for user_data in dict_data:
         for i in range(8):
             news_id = "newsID_" + str(i)
@@ -38,12 +54,14 @@ def transform2excel(dict_data):
                 s_time = datetime.datetime.strptime(visit_d["startTime"], date_format)
                 e_time = datetime.datetime.strptime(visit_d["endTime"], date_format)
                 user_data[visited_id + "_duration"] = (e_time - s_time).seconds
-
-
+        uid = user_data["uid"]
+        if user_data["duration"] is not None:
+            if user_duration[uid]["duration_from_hb"] > user_data["duration"]:
+                print("user:{} record duration: {} < duration from hb: {}".format(uid, user_data["duration"], user_duration[uid]["duration_from_hb"]))
 
     # df = pd.read_json("../attachment/statisticalData.json")
     df = pd.DataFrame.from_dict(dict_data)
-    df.to_excel("../attachment/userData.xlsx")
+    df.to_excel("../attachment/userData_v2.xlsx")
     print(df.describe())
 
 
